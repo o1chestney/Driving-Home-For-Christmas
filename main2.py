7ee3f354bf819df30f486980321ba0bc98745a27
Olivia <115619413+11olivia26@users.noreply.github.com>
2023-12-14 09:11:09 +0000

    Commenting!

--- main2.py
+++ main2.py
@@ -1,64 +1,67 @@
+# Importing ---------------
 import pygame, sys
 
 from config import *
 from sprites import *
 from asset_loader import *
 
+# Running the game --------------------------------------------------------
 class Game:
     def __init__(self):
         self.running = True
 
-        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_WIDTH))
-        self.clock=pygame.time.Clock()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, # Setting the screen size
+                                              WIN_WIDTH)) # to fit the user's screen
+        self.clock=pygame.time.Clock() # For FPS and timers
 
-        self.asset_loader = Asset_Loader(self)
+        self.asset_loader = Asset_Loader(self) # For rendering assets
 
 
-    def draw(self):
-        self.screen.fill(ROAD_COLOUR)
+    def draw(self): # Drawing the items to the screen
+        self.screen.fill(ROAD_COLOUR) # Temporary background which is a single colour
 
-        self.all_sprites.draw(self.screen)
-        self.clock.tick(FPS)
-        pygame.display.update()
+        self.all_sprites.draw(self.screen) # Drawing sprites to the screen (car, obstacles)
+        self.clock.tick(FPS) # Setting the FPS
+        pygame.display.update() # Refreshing the screen           
 
-    def createTilemap(self):
-        for i, row in enumerate(tilemap):
+    def createTilemap(self): # Creating the tile map
+        for i, row in enumerate(tilemap): 
             for j, column in enumerate(row):
-                if column==1:
-                    Block(self, j, i)
-                if column == 2:
-                    Player(self, j, i)
+                if column==1: # If "1" in tilemap
+                    Block(self, j, i) # Draw obstacle
+                if column == 2: # If "2" in tilemap
+                    Player(self, j, i) # Draw player
 
-    def main_menu(self):
+    def main_menu(self): # For the menu
         pass
 
-    def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
+    def events(self): # For quitting
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: # If event = pressing the X button in top right
+                self.running = False      # Quit game
+            elif event.type == pygame.KEYDOWN:   # If key pressed
+                if event.key == pygame.K_ESCAPE: # And that key is escape, then:
+                    self.running = False         # Quit
 
-    def update(self):
-        self.all_sprites.update()
+    def update(self): # For refreshing the sprites
+        self.all_sprites.update() # Update sprites (position, colour, anything in the function!)
 
-    def main(self):
-        while self.running:
-            self.events()
-            self.draw()
-            self.update()
+    def main(self): # The main game
+        while self.running: # While app running
+            self.events()   # Call the functions
+            self.draw()     # above in this order
+            self.update()   # so the program works!
             
-    def new(self):
-        self.all_sprites = pygame.sprite.LayeredUpdates()
+    def new(self): # For things you want from the start at the game: e.g. player
+        self.all_sprites = pygame.sprite.LayeredUpdates() # Puts the sprites on screen
 
-        self.createTilemap()
+        self.createTilemap() # Creates a tilemap
 
-if __name__ == "__main__":
-    game = Game()
+if __name__ == "__main__": # If the name = the file you're running, will return True
+    game = Game() # Calling the game class
 
-    game.new()
-    game.main()
+    game.new()    # Calling the functions
+    game.main()   # inside the class
 
-    pygame.quit()
-    sys.exit()
+    pygame.quit() # Quits out of pygame once main stops running
+    sys.exit()    # Closes the window
